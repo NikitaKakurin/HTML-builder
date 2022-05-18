b304fdfcbb652a45adfdfcc1dc92753912d2b321
NikitaKakurin <neyasut87@rambler.ru>
2022-05-18 14:12:28 +0300

    feat: add solution with variable data

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const pathToFile = path.join(__dirname, 'text.txt');
+console.log(pathToFile);
+const readStream = fs.createReadStream(pathToFile, 'utf-8');
+
+let data = '';
+readStream.on('data', (chunk) => data += chunk);
+readStream.on('end', () => console.log(data));
+readStream.on('error', (error) => console.log('Error:' + error.message));
