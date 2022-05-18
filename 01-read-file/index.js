669b9dacab752302585abaec3a55e8f46551d7ea
NikitaKakurin <neyasut87@rambler.ru>
2022-05-18 14:39:47 +0300

    feat: add solution without variable data

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const pathToFile = path.join(__dirname, 'text.txt');
-console.log(pathToFile);
+
 const readStream = fs.createReadStream(pathToFile, 'utf-8');
 
-let data = '';
-readStream.on('data', (chunk) => data += chunk);
-readStream.on('end', () => console.log(data));
+readStream.on('data', (chunk) => console.log(chunk));
 readStream.on('error', (error) => console.log('Error:' + error.message));
