87bb2a501a4f96548f5559d3fb2ca0e006758750
NikitaKakurin <neyasut87@rambler.ru>
2022-05-22 21:37:21 +0300

    fix: add solution#5 with reduce

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,36 +1,59 @@
 const fs = require('fs');
 const path = require('path');
-const targetFilesPath = path.join(__dirname, 'project-dist');
+const projectDist = path.join(__dirname, 'project-dist');
 const sourceFilesPath = path.join(__dirname, 'styles');
 (async() => {
-  let filesTarget;
-  try{
-    filesTarget = await fs.promises.readdir(targetFilesPath);
-    filesTarget.forEach(async(file)=>{
-      if(file !== 'index.html'){
-        return await fs.promises.unlink(path.join(targetFilesPath, file));
-      }    
-    });
-  } catch(err){
-    console.log('cleanTargetError ' + err.message);
+
+  async function clearFolder(folder) {
+    try{
+      const filesTarget = await fs.promises.readdir(folder);
+      const prom = filesTarget.reduce(async(acc,file)=>{
+        const promise = await acc;
+        const targetPath = path.join(folder, file);
+        const stats = await fs.promises.stat(targetPath);
+        if(stats.isDirectory()){
+          return await fs.promises.rm(targetPath, { recursive: true, force: true });
+        }
+        if(file !== 'index.html'){
+          return await fs.promises.unlink(targetPath);
+        }    
+      });
+      return prom;
+    } catch(err){
+      console.log('cleanTargetError ' + err.message);
+    }
   }
+  await clearFolder(projectDist);
 
 
-  let filesSource;
-  try{
-    filesSource = await fs.promises.readdir(sourceFilesPath);
-    filesSource.forEach(async (file) => {
-      const sourceFilePath = path.join(sourceFilesPath, file);
-      if(path.parse(sourceFilePath).ext === '.css'){
-        const readStream = await fs.createReadStream(sourceFilePath, 'utf-8');
-        readStream.on('data', async (chunk) => {
-          return await fs.promises.appendFile(path.join(targetFilesPath, 'bundle.css'), chunk);
-        });
-      }
-    });
-  } catch(err){
-    console.log('readSourceError ' + err.message);
-  }  
+  async function bundleCss(folder) {
+  
+    try{
+      const readCss = async function () {
+        const filesSource = await fs.promises.readdir(folder);
+        let cssText = filesSource.reduce(async (text, file) => {
+          const sourceFilePath = path.join(folder, file);
+          const stats = await fs.promises.stat(sourceFilePath);
+          const allCss = await text;
+          if(stats.isDirectory()) return allCss;        
+          if(path.parse(sourceFilePath).ext !== '.css')return allCss;
+
+          const context = await fs.promises.readFile(sourceFilePath, 'utf-8');          
+          if(context.includes('@font-face {')){
+            return context  + '\n' + allCss ;
+          }
+          return allCss + context + '\n';
+          
+        },'');
+        return cssText;
+      };
+      const styles = await readCss();
+      return await fs.promises.writeFile(path.join(projectDist, 'bundle.css'), styles);
+    } catch(err){
+      console.log('readSourceError ' + err.message);
+    }  
+  }
+  await bundleCss(sourceFilesPath);
 
 })();
 
