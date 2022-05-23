5b1ff1867f55f07a0c6d87f2aa5101b9647a86fe
NikitaKakurin <neyasut87@rambler.ru>
2022-05-23 10:14:35 +0300

    fix: fix task#4 add deep clone and change clearFiles

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,22 +3,36 @@ const path = require('path');
 const targetFilesPath = path.join(__dirname, 'files-copy');
 const sourceFilesPath = path.join(__dirname, 'files');
 (async() => {
+  async function clearFiles(folderPath) {
+    return await fs.promises.rm(folderPath, { recursive: true, force: true });
+  }
+  await clearFiles(targetFilesPath);
+
   await fs.promises.mkdir(targetFilesPath,{recursive:true});
 
-  const filesTarget = await fs.promises.readdir(targetFilesPath);
-  filesTarget.forEach(async(file)=>{
-    return await fs.promises.unlink(path.join(targetFilesPath, file));
-  });
-
-  let filesSource;
-  try{
-    filesSource = await fs.promises.readdir(sourceFilesPath);
-  } catch(err){
-    console.log('readdirError ' + err.message);
-  }  
-  filesSource.forEach(async(file) => {
-    return await fs.promises.copyFile(path.join(sourceFilesPath, file), path.join(targetFilesPath, file));
-  });
+  async function copyFiles(addFolderPath) {
+    let additionalPath = addFolderPath||'';
+    let pathToFile = path.join(sourceFilesPath,addFolderPath);
+    let filesSource;
+    try{
+      filesSource = await fs.promises.readdir(pathToFile);
+      const prom = filesSource.reduce(async(acc, file) => {
+        const promise = await acc;
+        const stats = await fs.promises.stat(path.join(pathToFile, file));
+        if(stats.isDirectory()){
+          await fs.promises.mkdir(path.join(targetFilesPath, additionalPath, file),{recursive:true});
+          return copyFiles(additionalPath + file);
+        }
+        // The promise is used to eliminate the error ESlint
+        promise;
+        return await fs.promises.copyFile(path.join(pathToFile, file), path.join(targetFilesPath, additionalPath, file));
+      },'');
+      return prom;
+    } catch(err){
+      console.log('copyError ' + err.message);
+    }  
+  }
+  await copyFiles('');
 })();
 
 
